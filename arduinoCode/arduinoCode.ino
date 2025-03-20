f2317cd1128a30b744511ac98d67487734ba1812
Djb604 <dominicboily604@gmail.com>
2025-03-19 18:38:46 -0700

    Update arduinoCode.ino
    ArduinoCode able to sync clock and recieve a bit every second

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,9 +1,29 @@
+#define ONLINE 0x02
+#define RECV 0x03
+#define SEND 0x04
+#define CLK 0x05
+  int value;
+  int clock;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(CLK, INPUT);
+  pinMode(RECV, INPUT);
+}
 
+void CheckClock() {
+  clock = digitalRead(CLK);
+
+  if (clock == 1) {
+    value = digitalRead(RECV);
+    Serial.println(value);
+    delay(1000);
+
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  CheckClock();
 }
