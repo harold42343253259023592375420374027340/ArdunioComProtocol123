70888ac176b3180ac8c5f442df63bacd8a6bd2b9
FinnH <finnlharrison@gmail.com>
2025-03-19 18:41:07 -0700

    got server some what working

--- grandcentralcode/grandcentralcode.ino
+++ grandcentralcode/grandcentralcode.ino
@@ -1,9 +1,34 @@
+
+#define ONLINE 0x02
+#define RECV 0x03
+#define SEND 0x04
+#define CLK 0x05
+
+int dataindex;
+int data[] = {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+class Clock {
+
+    public:
+      void toggle(int time) {
+        delay(time);
+        digitalWrite(CLK, 0);
+        delay(time);
+        digitalWrite(CLK, 1);
+        digitalWrite(SEND, data[dataindex]);
+        dataindex++;
+      }
+};
+Clock clock;
+
 void setup() {
   // put your setup code here, to run once:
-
+  int ips[256];
+  pinMode(CLK, OUTPUT);
+  pinMode(SEND, OUTPUT);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
+  clock.toggle(1000);
+
 
 }
